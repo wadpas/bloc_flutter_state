0656adf139a0efbddb30dc4f1490d332dad710f1
wadpas <wadpas@gmail.com>
2023-10-09 11:37:33 +0300

    added EmailTextField widget

---
+++ lib/views/email_text_field.dart
@@ -0,0 +1,21 @@
+import 'package:bloc_flutter_state/models/strings.dart';
+import 'package:flutter/material.dart';
+
+class EmailTextField extends StatelessWidget {
+  final TextEditingController emailController;
+
+  const EmailTextField({
+    super.key,
+    required this.emailController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      autocorrect: false,
+      decoration: const InputDecoration(hintText: enterYourEmailHere),
+    );
+  }
+}
